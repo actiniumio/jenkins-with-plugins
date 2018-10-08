bd968c9f7d50dfb54e409ddd871741eabca71abc
Germain <germain.gau@gmail.com>
2018-10-08 11:40:35 +0200

    added docker hub image push

--- build_images.sh
+++ build_images.sh
@@ -3,5 +3,6 @@ set -xeu pipefail
 while read version; do
   echo "Building for Jenkins version : $version"
   docker build -t jenkins --build-arg JENKINS_VERSION=$version .
-  docker tag jenkins actiniumio/jenkins:$version
+  docker tag jenkins $DOCKER_USER/jenkins:$version
+  docker push $DOCKER_USER/jenkins:$version
 done < tags.txt
