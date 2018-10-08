75d21d49d00f8734dc3d9377115e14b62253446f
Germain <germain.gau@gmail.com>
2018-10-08 11:43:59 +0200

    Suppressed docker build output

--- build_images.sh
+++ build_images.sh
@@ -2,7 +2,7 @@ set -xeu pipefail
 
 while read version; do
   echo "Building for Jenkins version : $version"
-  docker build -t jenkins --build-arg JENKINS_VERSION=$version .
+  docker build -q -t jenkins --build-arg JENKINS_VERSION=$version .
   docker tag jenkins $DOCKER_USER/jenkins:$version
   docker push $DOCKER_USER/jenkins:$version
 done < tags.txt
