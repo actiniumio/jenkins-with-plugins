d325db7622b8dbd535e2115c26557d24803bc99a
Germain <germain.gau@gmail.com>
2018-10-20 21:48:03 +0200

    feature(status_infos+push_option)

--- build_images.sh
+++ build_images.sh
@@ -1,10 +1,16 @@
-set -xeu pipefail
+set -euo pipefail
 
+i=1
+mx=$(wc -l tags.txt | cut -d' ' -f1)
 while read version; do
-  echo "Building for Jenkins version : $version"
+  echo "[$i / $mx] Building for Jenkins version : $version"
+  i=$((i+1))
   docker build -q -t jenkins --build-arg JENKINS_VERSION=$version .
-  docker tag jenkins $DOCKER_USER/jenkins:$version
-  docker push $DOCKER_USER/jenkins:$version
+  docker tag jenkins ${DOCKER_USER:-"actiniumio"}/jenkins:$version
+  if [ $# -gt 0 ] && [ $1 == "--push" ]
+  then
+    docker push $DOCKER_USER/jenkins:$version
+  fi
 done < tags.txt
 tac tags.txt > tags_ordered.txt
 mv tags_ordered.txt tags.txt
