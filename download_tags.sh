65fb787e13ab0958c147974f9263d9647d36884e
Germain <germain.gau@gmail.com>
2018-10-20 21:46:51 +0200

    feature(status_informations)

--- download_tags.sh
+++ download_tags.sh
@@ -1,4 +1,4 @@
-set -xeu pipefail
+set -euo pipefail
 
 rm -f tags.txt
 
@@ -7,16 +7,19 @@ repository="jenkins/jenkins"
 function download_page() {
   url=$1
 
+  echo "[Download] $url"
   curl -s $url -o tags.raw.json
   cat tags.raw.json | jq -cMr '.results[].name' >> tags.txt
 }
 
 download_page "https://hub.docker.com/v2/repositories/$repository/tags/"
 next=$(cat tags.raw.json | jq -cMr .next)
+page=1
 
-while [ "$next" != "null" ]
+while [ "$next" != "null" ] && [ "${MAX_PAGES:-"50"}" -gt "$page" ]
 do
   download_page $next
+  page=$((page+1))
   next=$(cat tags.raw.json | jq -cMr .next)
 done
 
@@ -25,3 +28,8 @@ rm -f tags.raw.json
 
 echo "$repository tags updated :"
 cat tags.txt
+
+echo ""
+echo "Summary:"
+echo " - Pages fetched   : $page"
+echo " - Tags discovered : $(wc -l tags.txt)"
